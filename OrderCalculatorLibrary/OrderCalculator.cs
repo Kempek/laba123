ba8d7d8d28001fa80fcb0f89a987ab0e7187ed7f
Kempek <egar.kill@gmail.com>
2024-12-24 04:37:36 +0300

    1

---
+++ OrderCalculatorLibrary/OrderCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderCalculatorLibrary
+{
+    public class OrderCalculator
+    {
+        public static double CalculateTotalOrderCost(double unitPrice, int quantity, double discount)
+        {
+            return unitPrice * quantity * (1 - discount);
+        }
+    }
+}
